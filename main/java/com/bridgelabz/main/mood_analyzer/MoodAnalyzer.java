e61224684e771f054625ed761d0a3f87dfca52f9
Shubham Rana <ranashubham7296@gmail.com>
2020-10-24 10:58:47 +0530

    Refactored: Changed to use constructor arg for passing mood message

--- main/java/com/bridgelabz/main/mood_analyzer/MoodAnalyzer.java
+++ main/java/com/bridgelabz/main/mood_analyzer/MoodAnalyzer.java
@@ -1,17 +1,28 @@
 package com.bridgelabz.main.mood_analyzer;
 
 public class MoodAnalyzer {
+	
+	private String message;
+	
+	//Parameterized constructor
+	public MoodAnalyzer(String message) 
+	{
+		this.message = message;
+	}
+
+	//Default constructor
+	public MoodAnalyzer() 
+	{}
 
 	public static void main(String[] args) 
 	{
-		MoodAnalyzer mood = new MoodAnalyzer();
-		String feel = mood.analyzeMood("I am Sad");
-		System.out.println(feel);
+		MoodAnalyzer mood = new MoodAnalyzer("I am sad");
+		mood.analyzeMood();
 	}
 
-	private String analyzeMood(String message) 
-	{
-		if(message.contains("sad"))
+	private String analyzeMood() 
+	{		
+		if(this.message.contains("sad"))
 			return "SAD";
 		else
 			return "HAPPY";
